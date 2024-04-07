b1b9cbf7305b997e81704e17855e1c6143316854
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2024-04-07 18:06:06 -0400

    Update Week_6_Recursion2.py

--- Week_6_Recursion2.py
+++ Week_6_Recursion2.py
@@ -1,9 +1,11 @@
+# Countdown Example
 def countdown(n):
     for i in range(n, -1, -1):
         print(i)
 
 countdown(5)
 
+# Add Up Numbers without Recursion
 def addupNumbers(n):
     total = 0
 
@@ -12,4 +14,13 @@ def addupNumbers(n):
 
     return total
 
-print(addupNumbers(100))
+print(addupNumbers(100)) # 5050
+
+# Add Up Numbers with Recursion
+def addupNumbers(n):
+    if n == 1:
+        return 1
+
+    return n + addupNumbers(n-1)
+
+print(addupNumbers(100)) # 5050
